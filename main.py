02c3b23ed667756b80b1a21dc78bd2bf5ef24406
Abdullah <heighter79@gmail.com>
2023-11-24 23:10:13 +0500

    day 3 completing 80%

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from account_credentials import username, password
 from targetted_account import target_acc
-from time import sleep as time_sleep
+from time import sleep as time_sleep, time
+
 
 class FollowerBot:
 
-    def __init__(self,username,password, target_acc):
-
+    def __init__(self, username, password, target_acc):
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=self.chrome_options)
@@ -34,22 +34,24 @@ class FollowerBot:
             EC.presence_of_element_located((By.NAME, "password"))
         )
         input_password.send_keys(self.password)
-        click_login = self.driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[3]/button/div')
+        click_login = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
         click_login.click()
 
     def target_account(self):
         self.driver.get(f"https://www.instagram.com/{self.target_acc}")
 
     def follow_accounts(self):
-        follower_count_button=self.driver.find_element(By.XPATH,'//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
-        follower_count_button.click()
+        link = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH,
+                                        "//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']"))
+        )
+        link.click()
 
 
-d = FollowerBot("businessgenerate","Allah78666","businessdrivendream")
+
+d = FollowerBot("businessgenerate", "Allah78666", "businessdrivendream")
 d.instagram()
 d.login()
 WebDriverWait(d.driver, 10).until(EC.url_changes("https://www.instagram.com/"))
 d.target_account()
-WebDriverWait(d.driver, 10).until(EC.url_changes("https://www.instagram.com/"))
 d.follow_accounts()
-
