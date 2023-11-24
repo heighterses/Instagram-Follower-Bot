7d8ea330cc79e0842fa7b6d61650e2c61642104e
Abdullah <heighter79@gmail.com>
2023-11-24 23:20:21 +0500

    day 3 completing 82%

--- main.py
+++ main.py
@@ -41,11 +41,15 @@ class FollowerBot:
         self.driver.get(f"https://www.instagram.com/{self.target_acc}")
 
     def follow_accounts(self):
-        link = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                                        "//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']"))
+        counter_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']"))
         )
-        link.click()
+        counter_button.click()
+        follow_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(@class, '_ap30')]"))
+        )
+        for n in follow_button:
+            n.click()
 
 
 
