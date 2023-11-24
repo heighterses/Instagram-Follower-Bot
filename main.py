36d08ada8c32f679ce71272c818f2e9b8a158fca
Abdullah <heighter79@gmail.com>
2023-11-24 10:36:35 +0500

    day 3 fixing bugs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -41,33 +40,16 @@ class FollowerBot:
     def target_account(self):
         self.driver.get(f"https://www.instagram.com/{self.target_acc}")
 
+    def follow_accounts(self):
+        follower_count_button=self.driver.find_element(By.XPATH,'//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        follower_count_button.click()
+
 
 d = FollowerBot("businessgenerate","Allah78666","businessdrivendream")
 d.instagram()
 d.login()
 WebDriverWait(d.driver, 10).until(EC.url_changes("https://www.instagram.com/"))
 d.target_account()
-
-
-
-
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(options=chrome_options)
-#
-# driver.get("https://www.instagram.com/")
-#
-# driver.implicitly_wait(5)
-#
-#
-#
-# input_username = driver.find_element(By.NAME, 'username')
-# input_username.send_keys(username)
-# input_password = driver.find_element(By.NAME, 'password')
-# input_password.send_keys(password)
-# login_button = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
-# login_button.click()
-# driver.get(f"https://www.instagram.com/{target_acc}")
-
+WebDriverWait(d.driver, 10).until(EC.url_changes("https://www.instagram.com/"))
+d.follow_accounts()
 
