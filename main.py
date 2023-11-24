0bd915039e9ab73a17b44cf0b24d4fd69e6125aa
Abdullah <heighter79@gmail.com>
2023-11-24 23:30:02 +0500

    Bot completed 100%

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 # ------------where the algorithm begins------------------
 
 
@@ -40,15 +41,16 @@ class FollowerBot:
 
     def follow_accounts(self):
         counter_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']"))
+            EC.element_to_be_clickable((By.XPATH,
+                                        "//a[@class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']"))
         )
         counter_button.click()
-        follow_buttons=WebDriverWait(self.driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(@class, '_ap30')]"))
-            )
+        follow_buttons = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH,
+                                                 "//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(@class, '_ap30')]"))
+        )
         for button in follow_buttons:
             button.click()
-
 
 
 insta_Bot = FollowerBot("businessgenerate", "Allah78666", "businessdrivendream")
